280e5f0efd4acb63da70cee081c7e2c4c2b39190
kirill <kirillbelov326@gmail.com>
2025-12-24 13:48:40 +0300

    циклы

---
+++ Основы алгоритмизации и программирования/Циклы/упр4/упр4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a >= 0 && a <= 10 && b >= 0 && b <= 10)
+            {
+                Console.WriteLine("Результат: " + (a * b));
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Числа недопустимы, попробуйте снова.");
+            }
+        }
+    }
+}
